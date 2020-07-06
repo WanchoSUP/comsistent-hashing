800728efda33d07344d67e454c658cfdd9f998b5
Wancho <as6747527@163.com>
2020-07-06 23:39:00 +0800

    add distribution standard deviation calculation

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <random>
 #include <map>
 #include <vector>
+#include <cmath>
 
 #include "ConsistentHashing.h"
 
@@ -11,14 +12,18 @@ using namespace ConsistenHashing;
 
 int main(int argc, char **argv)
 {
-    // init params
+    // Host IPs
     vector<string> nodeList{
-        "192.168.100.1",
-        "192.168.100.2",
-        "192.168.100.3",
-        "192.168.100.4",
-        "192.168.100.5",
-        "192.168.100.6"};
+        "192.168.100.101",
+        "192.168.100.102",
+        "192.168.100.103",
+        "192.168.100.104",
+        "192.168.100.105",
+        "192.168.100.106",
+        "192.168.100.107",
+        "192.168.100.108",
+        "192.168.100.109",
+        "192.168.100.110"};
 
     // Create a hash ring
     HashRing hashRing;
@@ -27,32 +32,38 @@ int main(int argc, char **argv)
         hashRing.addNode(node, 200);
     }
 
-    // test
-    for (int index = 0; index < nodeList.size(); index++)
+    //
+    map<string, int> counting;
+    for (auto &node : nodeList)
     {
-        //
-        map<string, int> counting;
-        for (auto &node : nodeList)
-        {
-            counting.emplace(node, 0);
-        }
-
-        for (int i = 0; i < 100'000; i++)
-        {
-            auto hitted = counting.find(hashRing.getNode(to_string(random())));
-            hitted->second++;
-        }
-
-        cout << "round(s) " << index + 1 << ":" << endl;
-        for (auto &item : counting)
-        {
-            cout << item.first << ", hits: " << item.second << endl;
-        }
-
-        string toremove = nodeList[index];
-
-        hashRing.removeNode(toremove);
+        counting.emplace(node, 0);
     }
+
+    // Hitting test
+    for (int i = 0; i < 1000'000; i++)
+    {
+        string dataKey = to_string(random());
+        string nodeKey = hashRing.getNode(dataKey);
+
+        auto hittedItem = counting.find(nodeKey);
+        hittedItem->second++;
+    }
+
+    // Calculate standard deviation
+    double sum = 0;
+    for_each(counting.begin(), counting.end(), [&](decltype(counting)::const_reference item) { sum += item.second; });
+    double mean = sum / counting.size();
+    double variance = 0.;
+    for_each(counting.begin(), counting.end(), [&](decltype(counting)::const_reference item) { variance += pow(item.second - mean, 2.); });
+    variance = variance / counting.size();
+    double standardDeviation = sqrt(variance);
+
+    // Output results
+    for (auto &item : counting)
+    {
+        cout << item.first << ", hits: " << item.second << endl;
+    }
+    cout << "Standard deviation: " << standardDeviation << endl;
 
     return 0;
 }
